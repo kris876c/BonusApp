5e9a37e521e8efc9f812cd4663ccd071873db421
kristoffer bøttcher <krisboettcher@live.dk>
2018-12-14 10:34:15 +0100

    jhg

---
+++ BonusApp/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Order
+    {
+        public BonusProvider Bonus { get; set; }
+        public List<Product> order = new List<Product>();
+        
+        public void AddProduct(Product p)
+        {
+            order.Add(p); 
+        }
+        public double GetValueOfProducts()
+        {
+            double result = 0;
+            int i = 0;
+            while(order.Count > i)
+            {
+                double orderValue = order[i].Value;
+                result = result + orderValue;
+                i++;
+            }
+            return result;
+        }
+        public double GetBonus()
+        {
+            double valueAll = GetValueOfProducts();
+            
+            return 0;
+        }
+        public double GetTotalPrice()
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("651ab47f-2df7-445b-9794-c541588df3bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
